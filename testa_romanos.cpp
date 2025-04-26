87cfaaee9d817b5f41cc8a808d317ea9d5631348
Arthur <arthur.henrique529@gmail.com>
2025-04-26 12:16:40 -0300

    otimizando testes unitarios

--- testa_romanos.cpp
+++ testa_romanos.cpp
@@ -2,19 +2,19 @@
 #include "catch.hpp"
 #include "romanos.hpp"
 
-TEST_CASE("Verificacao de números romanos") {
+TEST_CASE("Numerais romanos válidos") {
     ConversorRomano conv;
     REQUIRE(conv.converter("I") == 1);
     REQUIRE(conv.converter("V") == 5);
     REQUIRE(conv.converter("XX") == 20);
     REQUIRE(conv.converter("IV") == 4);
-    REQUIRE(conv.converter("x") == 10);
+    REQUIRE(conv.converter("X") == 10);
+}
+
+TEST_CASE("Numerais romanos inválidos") {
+    ConversorRomano conv;
     REQUIRE(conv.converter("A") == -1);
     REQUIRE(conv.converter("IIII") == -1);
     REQUIRE(conv.converter("VX") == -1);
     REQUIRE(conv.converter("MMMD") == -1);
-
-
-
-
 }
