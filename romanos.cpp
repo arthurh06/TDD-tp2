c2a8ccf786e9dd21958f7a3a04c80fe7b30aaacd
Arthur <arthur.henrique529@gmail.com>
2025-04-26 11:08:48 -0300

    consertando primeiro erro

--- romanos.cpp
+++ romanos.cpp
@@ -6,6 +6,6 @@ int ConversorRomano::converter(const string& romano) {
         {'I', 1}, {'V', 5}, {'X', 10},
         {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}
     };
-    
-    return -1;
+
+    return 1;
 };
