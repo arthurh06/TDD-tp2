f573214ffa95a2e76d15d4f5b46f62de67733f0b
Arthur <arthur.henrique529@gmail.com>
2025-04-26 11:35:53 -0300

    nao lê entradas de numeros minusculos

--- testa_romanos.cpp
+++ testa_romanos.cpp
@@ -8,5 +8,7 @@ TEST_CASE("Números romanos válidos") {
     REQUIRE(conv.converter("V") == 5);
     REQUIRE(conv.converter("XX") == 20);
     REQUIRE(conv.converter("IV") == 4);
+    REQUIRE(conv.converter("x") == 10);
+
 
 }
