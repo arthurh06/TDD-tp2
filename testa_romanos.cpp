89cec261c9a599711af2ff0056c1f316b527e456
Arthur <arthur.henrique529@gmail.com>
2025-04-29 20:35:37 -0300

    mais testes realizados

--- testa_romanos.cpp
+++ testa_romanos.cpp
@@ -9,6 +9,11 @@ TEST_CASE("Numerais romanos válidos") {
     REQUIRE(conv.converter("XX") == 20);
     REQUIRE(conv.converter("IV") == 4);
     REQUIRE(conv.converter("X") == 10);
+    REQUIRE(conv.converter("XXX") == 30);
+    REQUIRE(conv.converter("XXV") == 25);
+    REQUIRE(conv.converter("XIV") == 14);
+    REQUIRE(conv.converter("II") == 2);
+    REQUIRE(conv.converter("III") == 3);
 }
 
 TEST_CASE("Numerais romanos inválidos") {
